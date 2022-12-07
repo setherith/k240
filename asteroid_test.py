1e5170bf590b8f2a37adccd3f66346a384b849d5
Shane Pudner <setherith@gmail.com>
2022-12-07 20:08:36 +0000

    Added buttons to screen for scale test

--- asteroid_test.py
+++ asteroid_test.py
@@ -6,6 +6,9 @@ height = 20
 
 app = Ursina()
 
+red_button = Entity(model='quad', texture='resources/red_button.png', position=(0.0225,0.022,0), scale=0.004, parent=camera.ui)
+blue_button = Entity(model='quad', texture='resources/blue_button.png', position=(0.0225,-0.022,0), scale=0.004, parent=camera.ui)
+
 # generate star field...
 import star_field
 sf_width = 5000
@@ -20,6 +23,7 @@ for w in range(0, width):
         size = int(uniform(1, 5))
         gray = int(uniform(50, 100))
         Entity(model='cube', scale_y=size, position=(w,0 - size / 2,h), color=color.rgb(gray, gray, gray))
+
 
 #EditorCamera()
 camera.rotation = Vec3(10, 225, 0)
